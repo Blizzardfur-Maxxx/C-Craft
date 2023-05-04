235e4cacd27d41c2fd38a63f78c12fb0f110ab68
Blizzardfur-Maxxx <111100393+Blizzardfur-Maxxx@users.noreply.github.com>
2023-05-03 20:27:09 -0500

    WOO NAMES WORK

--- C-Craft.c
+++ C-Craft.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <string.h>  
 
 char answer;
 
 
 int main()
 {
-    char name[50];
-    char command[100];
+    char name[] = "";
+    char runstring[] = "";
+    char run[] = "java -Djava.library.path=lib -Xmx1024M -cp lib\\lwjgl.jar;lib\\lwjgl_util.jar;lib\\jinput.jar;lib\\minecraft.jar net.minecraft.client.Minecraft ";
     printf("-=Weclome to C-Craft made by:Maxxx#5509=-\n");
     printf("what do you want to do:\n");
     printf("-----------------------\n");
@@ -29,9 +31,9 @@ int main()
     }
         if (answer == 'r')
         printf("what username do you want: ");
-        scanf("%s",&name);
-        sprintf(command, "java -Djava.library.path=lib -Xmx1024M -cp lib\\lwjgl.jar;lib\\lwjgl_util.jar;lib\\jinput.jar;lib\\minecraft.jar net.minecraft.client.Minecraft %s" , name);
-        system(command);
+        scanf("%s", &name);
+        strcat(run,name);
+        system(run);
     {
     }
         if (answer == 'q')
